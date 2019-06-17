397ecf02542c16ef61404280ebce35905886cf17
Bruno Ramos Toresan <42551829+brunotoresan@users.noreply.github.com>
2019-06-17 15:28:15 -0300

    Update t2fs.c

--- t2fs/src/t2fs.c
+++ t2fs/src/t2fs.c
@@ -209,7 +209,7 @@ int readdir2 (DIR2 handle, DIRENT2 *dentry) {
 	}
 	
 	// Check if the handle is valid
-	if ((handle >= 0) && (handle < 9))
+	if ((handle < 0) || (handle > 9))
 	{
 	  strcpy(dentry->name, open_files[handle].registro.name);
 	  dentry->fileType = open_files[handle].registro.file_type;
